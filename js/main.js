b0e74514801077a254d24059b64569216293fb8a
Asher <il08082000@gmail.com>
2019-10-24 01:06:53 +0700

    добавлен футер +нужные скрипты

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(window).load(function() {
+    $('.before-after').twentytwenty({
+        before_label: 'Без скинали',
+        after_label: 'Со скинали',
+    });
+    $('.before-slider').slick({
+        draggable: false,
+        dots:true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow_left'),
+        nextArrow: $('.arrow_right')
+    });
+    $('.menu-button').on('click', function(){
+        $('.menu').toggleClass('menu_active');
+    });
+    $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_opened');
+    });
+    $('.select__option').on('click', function(){
+        var value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_opened');
+    });
+});
